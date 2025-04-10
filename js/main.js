bc44087e0146ad50a5f94e96fda13364c358c5ee
Samantha Garcia <sgm9705@gmail.com>
2025-04-10 15:59:28 -0600

    actividad 6

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ const productosTotal = document.getElementById("productosTotal");
 let cont = 0;
 let costoTotal = 0;
 let totalEnProductos = 0;
+let datos = []; //new Array();
 
 
 function validarCantidad(){
@@ -81,6 +82,18 @@ btnAgregar.addEventListener("click", function (event){
                     <td> ${txtNumber.value}</td>
                     <td> ${precio} </td>
                     </tr>`;
+
+            let elemento = { // Aqui creamos un objeto  "nombre de" : valor. Es formato json
+                "cont" : cont,
+                "nombre" : txtName.value,
+                "cantidad" : txtNumber.value,
+                "precio" : precio
+            };
+            datos.push(elemento); // lo guardamos (el objeto) en el array
+
+            localStorage.setItem("datos", JSON.stringify(datos)); // guardamos el array en el local storage en string
+
+         
         cuerpoTabla.insertAdjacentHTML("beforeend", row); //anade celddas nuevas hacia abajo
         
         contadorProductos.innerText = cont;
@@ -94,6 +107,37 @@ btnAgregar.addEventListener("click", function (event){
         txtName.focus(); // el puntero parpade en el name 
     };
 
+});
+
+
+window.addEventListener("load", function(event){
+    event.preventDefault();
+    
+    if(localStorage.getItem("datos")!=null){
+        datos = JSON.parse(localStorage.getItem("datos"));
+    }
+
+    datos.forEach((d) =>{
+        let row = `<tr>
+                     <td>${d.cont}</td>
+                     <td>${d.nombre}</td>
+                     <td>${d.cantidad}</td>
+                     <td>${d.precio}</td>
+                     </tr>`
+            cuerpoTabla.insertAdjacentHTML("beforeend", row);
+    });
+
+    if(this.localStorage.getItem("resumen")!=null){
+        let resumen = JSON.parse(this.localStorage.getItem("resumen"));
+
+        costoTotal = resumen.costoTotal;
+        totalEnProductos = resumen.totalEnProductos;
+        cont = resumen.cont;
+    }
+    precioTotal.innerText = "$" + costoTotal.toFixed(2);
+    productosTotal.innerText = totalEnProductos;
+    contadorProductos.innerText = cont;
+    
 });
 
 // Validamos la cantidad. - que sea numerico, que sea mayo a 0 
