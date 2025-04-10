639e7583f77238cd03e5665b07ee616fd871d8c0
Samantha Garcia <sgm9705@gmail.com>
2025-04-10 13:03:17 -0600

    actividades hasta el punto 5

--- js/main.js
+++ js/main.js
@@ -2,23 +2,99 @@
 //id=variable
 //si en un imput pido valor. trim es para quitar los espacios
 
-let txtName = document.getElementById("Name");
-let txtNumber = document.getElementById("Number");
-let btnAgregar = document.getElementById("btnAgregar");
-let alertValidacionesTexto = document.getElementById("alertValidacionesTexto")
-let alertValidaciones = document.getElementById("alertValidaciones")
+const txtName = document.getElementById("Name");
+const txtNumber = document.getElementById("Number");
+const btnAgregar = document.getElementById("btnAgregar");
+const alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+const alertValidaciones = document.getElementById("alertValidaciones");
+const tablaListaCompras = document.getElementById("tablaListaCompras");
+const cuerpoTabla = tablaListaCompras.getElementsByTagName("tbody").item(0); //tagName contiene varios elementos, por eso se usa item; para especificar el elemento
+const contadorProductos = document.getElementById("contadorProductos");
+const totalProductos = document.getElementById("totalProductos");
+const productosTotal = document.getElementById("productosTotal");
 
+// Numeracion de la primera columna de la tabla 
+let cont = 0;
+let costoTotal = 0;
+let totalEnProductos = 0;
+
+
+function validarCantidad(){
+    if(txtNumber.value.trim().length<=0){ //la longitud es mayor a 0 
+        return false;
+    }
+    if(isNaN(txtNumber.value)){
+        return false;
+    }
+   
+    if(Number(txtNumber.value)<=0){ //lo convierte en numero y tiene que ser mayor o igual a 0
+        return false;
+
+    }
+    return true;
+};// validar cantidad 
+
+//precio por producto al azar
+
+function getPrecio(){
+    return Math.round((Math.random() *10000)) / 100;
+    
+};
+console.log(getPrecio);
+
+// Validamos el nombre
 btnAgregar.addEventListener("click", function (event){
 
     event.preventDefault();
 
+    let isValid = true; // benadera para verificar que las validaciones se cumplen 
+
+    txtName.style.border = "";
+    txtNumber.style.border = "";
+    alertValidacionesTexto.innerHTML = "";
+    alertValidaciones.style.display="none"
+    
     txtName.value = txtName.value.trim();
     txtNumber.value = txtNumber.value.trim();
 
     if(txtName.value.length <3){
         txtName.style.border = "solid medium red";
-        alertValidacionesTexto.innerHTML = "<strong> El nombre del producto no es correcto</strong>";
+        alertValidacionesTexto.innerHTML += "<strong> El nombre del producto no es correcto</strong>";
         alertValidaciones.style.display="block"
-    }
+        isValid=false; // es falso cuando no cumple las validaciones 
+    };
+
+    if (! validarCantidad()){ //si no es valida la cantidad
+        console.log("Cantidad inválida:", txtNumber.value);
+            txtNumber.style.border = "solid medium red";
+            alertValidacionesTexto.innerHTML += "<br><strong> La cantidad no es correcta</strong>"; //+= trae los dos textos si ambos estan mal 
+            alertValidaciones.style.display="block"
+            isValid=false;
+    };
+
+    if (isValid){
+        cont++;
+        let precio = getPrecio();
+        let row = `<tr>
+                    <td>${cont}</td>
+                    <td> ${txtName.value}</td>
+                    <td> ${txtNumber.value}</td>
+                    <td> ${precio} </td>
+                    </tr>`;
+        cuerpoTabla.insertAdjacentHTML("beforeend", row); //anade celddas nuevas hacia abajo
+        
+        contadorProductos.innerText = cont;
+       costoTotal+= precio * Number (txtNumber.value);
+       precioTotal.innerText = "$" + costoTotal.toFixed(2);
+       totalEnProductos+= Number (txtNumber.value);
+       productosTotal.innerText = totalEnProductos;
+       
+        txtName.value = "";
+        txtNumber.value = ""; 
+        txtName.focus(); // el puntero parpade en el name 
+    };
 
 });
+
+// Validamos la cantidad. - que sea numerico, que sea mayo a 0 
+
